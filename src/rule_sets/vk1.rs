210c92f90a61e57b8ab8da600163ea16c43af6e5
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 20:38:20 +0900

    refactor

--- src/rule_sets/vk1.rs
+++ src/rule_sets/vk1.rs
@@ -1,89 +1,72 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
 
 pub fn manipulators() -> Vec<Manipulator> {
-    vec![
-        vec![
-            (K::H, K::LeftArrow),
-            (K::J, K::DownArrow),
-            (K::K, K::UpArrow),
-            (K::L, K::RightArrow),
-        ]
-        .into_iter()
-        .map(|(from, to)| {
+    let mut manipulators = Vec::new();
+
+    for (from, to, modifiers) in [
+        (H, LeftArrow, None),
+        (J, DownArrow, None),
+        (K, UpArrow, None),
+        (L, RightArrow, None),
+        (F, Escape, None),
+    ] {
+        manipulators.push(
             Manipulator::builder()
                 .condition(Condition::with_vk1())
                 .from_key_with_modifiers(from, FromModifier::Optional(vec![Any]))
+                .to_key(to, modifiers)
+                .build(),
+        );
+    }
+
+    for (from, to) in [(S, JapaneseKana), (D, JapaneseEisuu), (A, F10), (Z, F7)] {
+        manipulators.push(
+            Manipulator::builder()
+                .condition(Condition::with_vk1())
+                .from_key(from)
                 .to_key(to, None)
-                .build()
-        })
-        .collect(),
+                .build(),
+        );
+    }
+
+    vec![
+        manipulators,
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::F, FromModifier::Optional(vec![Any]))
-                .to_key(K::Escape, None)
+                .from_key_with_modifiers(U, FromModifier::Optional(vec![Any]))
+                .to_key(LeftArrow, Some(vec![Cmd]))
+                .build(),
+            Manipulator::builder()
+                .condition(Condition::with_vk1())
+                .from_key_with_modifiers(I, FromModifier::Optional(vec![Any]))
+                .to_key(RightArrow, Some(vec![Cmd]))
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::S)
-                .to_key(K::JapaneseKana, None)
-                .build(),
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(K::D)
-                .to_key(K::JapaneseEisuu, None)
+                .from_key_with_modifiers(G, FromModifier::Optional(vec![Any]))
+                .to_key(Tab, None)
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::A)
-                .to_key(K::F10, None)
+                .from_key(O)
+                .to_key(Tab, Some(vec![Ctrl, Shift]))
                 .build(),
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::Z)
-                .to_key(K::F7, None)
-                .build(),
-        ],
-        vec![
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::U, FromModifier::Optional(vec![Any]))
-                .to_key(K::LeftArrow, Some(vec![Cmd]))
-                .build(),
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::I, FromModifier::Optional(vec![Any]))
-                .to_key(K::RightArrow, Some(vec![Cmd]))
+                .from_key(P)
+                .to_key(Tab, Some(vec![Ctrl]))
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::G, FromModifier::Optional(vec![Any]))
-                .to_key(K::Tab, None)
-                .build(),
-        ],
-        vec![
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(K::O)
-                .to_key(K::Tab, Some(vec![Ctrl, Shift]))
-                .build(),
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(K::P)
-                .to_key(K::Tab, Some(vec![Ctrl]))
-                .build(),
-        ],
-        vec![
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::Y, FromModifier::Mandatory(vec![Shift]))
-                .to_key(K::C, Some(vec![Cmd]))
+                .from_key_with_modifiers(Y, FromModifier::Mandatory(vec![Shift]))
+                .to_key(C, Some(vec![Cmd]))
                 .to_command(
                     "export LC_ALL=en_US.UTF-8; pbpaste | tr -d '\n' | sed 's/  */ /g' | pbcopy",
                 )
@@ -92,54 +75,54 @@ pub fn manipulators() -> Vec<Manipulator> {
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::Y)
-                .to_key(K::C, Some(vec![Cmd]))
+                .from_key(Y)
+                .to_key(C, Some(vec![Cmd]))
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::T)
-                .to_key(K::X, Some(vec![Cmd]))
+                .from_key(T)
+                .to_key(X, Some(vec![Cmd]))
                 .build(),
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::X)
-                .to_key(K::V, Some(vec![Cmd, Shift, Opt]))
-                .build(),
-        ],
-        vec![
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(K::C)
-                .to_key(K::DeleteOrBackspace, None)
-                .build(),
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(K::E)
-                .to_key(K::DeleteForward, None)
+                .from_key(X)
+                .to_key(V, Some(vec![Cmd, Shift, Opt]))
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key_with_modifiers(K::CloseBracket, FromModifier::Optional(vec![Any]))
-                .to_key(K::Z, Some(vec![Cmd]))
+                .from_key(C)
+                .to_key(DeleteOrBackspace, None)
+                .build(),
+            Manipulator::builder()
+                .condition(Condition::with_vk1())
+                .from_key(E)
+                .to_key(DeleteForward, None)
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::Quote)
-                .to_key(K::H, Some(vec![Cmd]))
+                .from_key_with_modifiers(CloseBracket, FromModifier::Optional(vec![Any]))
+                .to_key(Z, Some(vec![Cmd]))
+                .build(),
+        ],
+        vec![
+            Manipulator::builder()
+                .condition(Condition::with_vk1())
+                .from_key(Quote)
+                .to_key(H, Some(vec![Cmd]))
                 .build(),
         ],
         {
             let shift_mappings = vec![
-                (K::N, Some(-1536), None),
-                (K::M, None, Some(1536)),
-                (K::Comma, None, Some(-1536)),
-                (K::Period, Some(1536), None),
+                (N, Some(-1536), None),
+                (M, None, Some(1536)),
+                (Comma, None, Some(-1536)),
+                (Period, Some(1536), None),
             ]
             .into_iter()
             .map(|(key_code, x, y)| {
@@ -154,10 +137,10 @@ pub fn manipulators() -> Vec<Manipulator> {
                     .build()
             });
             let normal_mappings = vec![
-                (K::N, Some(-3072), None),
-                (K::M, None, Some(3072)),
-                (K::Comma, None, Some(-3072)),
-                (K::Period, Some(3072), None),
+                (N, Some(-3072), None),
+                (M, None, Some(3072)),
+                (Comma, None, Some(-3072)),
+                (Period, Some(3072), None),
             ]
             .into_iter()
             .map(|(key_code, x, y)| {
@@ -174,8 +157,8 @@ pub fn manipulators() -> Vec<Manipulator> {
             shift_mappings.chain(normal_mappings).collect()
         },
         vec![
-            (K::Slash, PointingButton::Button1),
-            (K::International1, PointingButton::Button2),
+            (Slash, PointingButton::Button1),
+            (International1, PointingButton::Button2),
         ]
         .into_iter()
         .map(|(from_key, pointing_button)| {
@@ -186,37 +169,33 @@ pub fn manipulators() -> Vec<Manipulator> {
                 .build()
         })
         .collect(),
+        vec![(OpenBracket, -64), (NonUsPound, 64), (Backslash, 64)]
+            .into_iter()
+            .map(|(key_code, vertical_wheel)| {
+                Manipulator::builder()
+                    .condition(Condition::with_vk1())
+                    .from_key(key_code)
+                    .to_mouse(MouseKey {
+                        x: None,
+                        y: None,
+                        vertical_wheel: Some(vertical_wheel),
+                    })
+                    .build()
+            })
+            .collect(),
         vec![
-            (K::OpenBracket, -64),
-            (K::NonUsPound, 64),
-            (K::Backslash, 64),
-        ]
-        .into_iter()
-        .map(|(key_code, vertical_wheel)| {
-            Manipulator::builder()
-                .condition(Condition::with_vk1())
-                .from_key(key_code)
-                .to_mouse(MouseKey {
-                    x: None,
-                    y: None,
-                    vertical_wheel: Some(vertical_wheel),
-                })
-                .build()
-        })
-        .collect(),
-        vec![
-            (K::Key1, K::F1),
-            (K::Key2, K::F2),
-            (K::Key3, K::F3),
-            (K::Key4, K::F4),
-            (K::Key5, K::F5),
-            (K::Key6, K::F6),
-            (K::Key7, K::F7),
-            (K::Key8, K::F8),
-            (K::Key9, K::F9),
-            (K::Key0, K::F10),
-            (K::Hyphen, K::F11),
-            (K::EqualSign, K::F12),
+            (Key1, F1),
+            (Key2, F2),
+            (Key3, F3),
+            (Key4, F4),
+            (Key5, F5),
+            (Key6, F6),
+            (Key7, F7),
+            (Key8, F8),
+            (Key9, F9),
+            (Key0, F10),
+            (Hyphen, F11),
+            (EqualSign, F12),
         ]
         .into_iter()
         .map(|(from, to)| {
@@ -230,15 +209,15 @@ pub fn manipulators() -> Vec<Manipulator> {
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::B)
-                .to_key(K::M, Some(vec![Ctrl, Opt, Cmd, Shift]))
+                .from_key(B)
+                .to_key(M, Some(vec![Ctrl, Opt, Cmd, Shift]))
                 .build(),
         ],
         vec![
             Manipulator::builder()
                 .condition(Condition::with_vk1())
-                .from_key(K::International3)
-                .to_key(K::D, Some(vec![Cmd, Opt]))
+                .from_key(International3)
+                .to_key(D, Some(vec![Cmd, Opt]))
                 .build(),
         ],
     ]
