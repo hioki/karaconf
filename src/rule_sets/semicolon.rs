4c513231524d53160968fce7c17a88b9dbaf4e4e
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 21:39:09 +0900

    refactor

--- src/rule_sets/semicolon.rs
+++ src/rule_sets/semicolon.rs
@@ -1,18 +1,18 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
 
 pub fn manipulators() -> Vec<Manipulator> {
     vec![
         Manipulator::builder()
-            .from_key_with_modifiers(K::Semicolon, FromModifier::Mandatory(vec![Ctrl]))
-            .to_key(K::Semicolon, None)
+            .from_key_with_modifiers(Semicolon, FromModifier::Mandatory(vec![Ctrl]))
+            .to_key(Semicolon, None)
             .build(),
         Manipulator::builder()
-            .from_key_with_modifiers(K::Semicolon, FromModifier::Mandatory(vec![Cmd, Shift]))
-            .to_key(K::KeypadPlus, Some(vec![Cmd]))
+            .from_key_with_modifiers(Semicolon, FromModifier::Mandatory(vec![Cmd, Shift]))
+            .to_key(KeypadPlus, Some(vec![Cmd]))
             .build(),
         Manipulator::builder()
-            .from_key(K::Semicolon)
-            .to_key(K::ReturnOrEnter, None)
+            .from_key(Semicolon)
+            .to_key(ReturnOrEnter, None)
             .build(),
     ]
 }
--- src/rule_sets/vk3.rs
+++ src/rule_sets/vk3.rs
@@ -1,18 +1,18 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
 
 pub fn manipulators() -> Vec<Manipulator> {
     vec![
-        (K::A, K::Key1),
-        (K::S, K::Key2),
-        (K::D, K::Key3),
-        (K::F, K::Key4),
-        (K::G, K::Key5),
-        (K::H, K::Key6),
-        (K::J, K::Key7),
-        (K::K, K::Key8),
-        (K::L, K::Key9),
-        (K::Semicolon, K::Key0),
-        (K::Quote, K::Hyphen),
+        (A, Key1),
+        (S, Key2),
+        (D, Key3),
+        (F, Key4),
+        (G, Key5),
+        (H, Key6),
+        (J, Key7),
+        (K, Key8),
+        (L, Key9),
+        (Semicolon, Key0),
+        (Quote, Hyphen),
     ]
     .into_iter()
     .map(|(from, to)| {
