789feed100366f7e46466dffc3d718509301fd02
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 20:11:44 +0900

    fix dynalist

--- src/rule_sets/dynalist.rs
+++ src/rule_sets/dynalist.rs
@@ -1,68 +1,42 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
+use std::collections::BTreeMap;
 
 pub fn manipulators() -> Vec<Manipulator> {
-    let vk1_conditions = vec![
-        Condition::on_app(BundleIdentifier::Dynalist),
-        Condition::with_vk1(),
-    ];
-    let vk2_conditions = vec![
-        Condition::on_app(BundleIdentifier::Dynalist),
-        Condition::with_vk2(),
-    ];
-    let vk4_conditions = vec![
-        Condition::on_app(BundleIdentifier::Dynalist),
-        Condition::with_vk4(),
-    ];
-    vec![
-        Manipulator::builder()
-            .conditions(vk1_conditions.clone())
-            .from_key(K::U)
-            .to_key(K::A, Some(vec![Ctrl]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk1_conditions.clone())
-            .from_key(K::I)
-            .to_key(K::E, Some(vec![Ctrl]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::E)
-            .to_key(K::F, Some(vec![Cmd, Shift]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::F)
-            .to_key(K::O, Some(vec![Cmd]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk2_conditions.clone())
-            .from_key(K::Key9)
-            .to_key(K::Hyphen, Some(vec![Cmd, Shift]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk2_conditions.clone())
-            .from_key(K::Key0)
-            .to_key(K::Hyphen, Some(vec![Cmd]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::J)
-            .to_key(K::DownArrow, Some(vec![Cmd]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::K)
-            .to_key(K::UpArrow, Some(vec![Cmd]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::H)
-            .to_key(K::Tab, Some(vec![Shift]))
-            .build(),
-        Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::L)
-            .to_key(K::Tab, None)
-            .build(),
-    ]
+    let m = BTreeMap::from([
+        (
+            VirtualKey::Vk1,
+            vec![
+                (U, A, Some(vec![Ctrl])), // go to line head
+                (I, E, Some(vec![Ctrl])), // go to line tail
+            ],
+        ),
+        (
+            VirtualKey::Vk2,
+            vec![
+                (Key9, Hyphen, Some(vec![Cmd, Shift])), // zoom in
+                (Key0, Hyphen, Some(vec![Cmd])),        // zoom out
+            ],
+        ),
+        (
+            VirtualKey::Vk4,
+            vec![
+                (E, F, Some(vec![Cmd, Shift])), // hide sidebar
+                (F, O, Some(vec![Cmd])),        // search
+            ],
+        ),
+    ]);
+    m.iter()
+        .flat_map(|(vk, mappings)| {
+            mappings.iter().map(move |(from, to, to_modifiers)| {
+                Manipulator::builder()
+                    .conditions(vec![
+                        Condition::on_app(BundleIdentifier::Dynalist),
+                        Condition::with_virtual_key(vk.clone()),
+                    ])
+                    .from_key(from.clone())
+                    .to_key(to.clone(), to_modifiers.clone())
+                    .build()
+            })
+        })
+        .collect::<Vec<Manipulator>>()
 }
