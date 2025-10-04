29d5a085be4941b60126b80163d70dac51370a99
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 20:31:24 +0900

    refactor

--- src/rule_sets/chatgpt.rs
+++ src/rule_sets/chatgpt.rs
@@ -1,15 +1,14 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
 
 pub fn manipulators() -> Vec<Manipulator> {
-    let vk4_conditions = vec![
-        Condition::on_app(BundleIdentifier::ChatGPT),
-        Condition::with_vk4(),
-    ];
     vec![
         Manipulator::builder()
-            .conditions(vk4_conditions.clone())
-            .from_key(K::E)
-            .to_key(K::S, Some(vec![Cmd, Ctrl]))
+            .conditions(vec![
+                Condition::on_app(BundleIdentifier::ChatGPT),
+                Condition::with_vk4(),
+            ])
+            .from_key(E)
+            .to_key(S, Some(vec![Cmd, Ctrl]))
             .build(),
     ]
 }
