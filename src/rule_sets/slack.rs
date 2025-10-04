8174b89d74c195b63e1a6ba0632602085ac511b0
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 20:13:08 +0900

    refactor

--- src/rule_sets/slack.rs
+++ src/rule_sets/slack.rs
@@ -1,27 +1,26 @@
-use crate::karabiner_data::{KeyCode as K, ModifierKey::*, *};
+use crate::karabiner_data::{KeyCode::*, ModifierKey::*, *};
 
 pub fn manipulators() -> Vec<Manipulator> {
-    let vk4_conditions = vec![
-        Condition::on_app(BundleIdentifier::Slack),
-        Condition::with_vk4(),
-    ];
     vec![
-        (K::T, K::T, vec![Cmd, Shift]),               // Threads
-        (K::U, K::A, vec![Cmd, Shift]),               // All Unreads
-        (K::E, K::D, vec![Cmd, Shift]),               // Toggle Sidebar
-        (K::K, K::G, vec![Cmd]),                      // Search
-        (K::F, K::K, vec![Cmd]),                      // Jump
-        (K::B, K::S, vec![Cmd, Shift]),               // Bookmarks
-        (K::D, K::X, vec![Cmd, Shift]),               // Strike through
-        (K::OpenBracket, K::C, vec![Cmd, Shift]),     // Code
-        (K::C, K::C, vec![Cmd, Opt, Shift]),          // Code Block
-        (K::Q, K::Key9, vec![Cmd, Shift]),            // Quote
-        (K::Spacebar, K::Spacebar, vec![Cmd, Shift]), // Mute huddle
+        (T, T, vec![Cmd, Shift]),               // Threads
+        (U, A, vec![Cmd, Shift]),               // All Unreads
+        (E, D, vec![Cmd, Shift]),               // Toggle Sidebar
+        (K, G, vec![Cmd]),                      // Search
+        (F, K, vec![Cmd]),                      // Jump
+        (B, S, vec![Cmd, Shift]),               // Bookmarks
+        (D, X, vec![Cmd, Shift]),               // Strike through
+        (OpenBracket, C, vec![Cmd, Shift]),     // Code
+        (C, C, vec![Cmd, Opt, Shift]),          // Code Block
+        (Q, Key9, vec![Cmd, Shift]),            // Quote
+        (Spacebar, Spacebar, vec![Cmd, Shift]), // Mute huddle
     ]
     .into_iter()
     .map(|(from, to, modifiers)| {
         Manipulator::builder()
-            .conditions(vk4_conditions.clone())
+            .conditions(vec![
+                Condition::on_app(BundleIdentifier::Slack),
+                Condition::with_vk4(),
+            ])
             .from_key(from)
             .to_key(to, Some(modifiers))
             .build()
