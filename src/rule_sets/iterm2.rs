4b8e60b13ab4c6aff66cdbc9e7d155bcd5293fa4
Daichi Hioki <hioki.daichi@gmail.com>
2025-10-04 19:59:42 +0900

    refactor

--- src/rule_sets/iterm2.rs
+++ src/rule_sets/iterm2.rs
@@ -14,7 +14,18 @@ const CMD: &[ModifierKey] = &[Cmd];
 pub fn manipulators() -> Vec<Manipulator> {
     let mut manipulators = Vec::new();
 
+    // Disable default Cmd+W behavior in iTerm2
+    manipulators.push(
+        Manipulator::builder()
+            .condition(Condition::on_app(ITerm2))
+            .from_key_with_modifiers(W, FromModifier::Mandatory(CMD.to_vec()))
+            .to_key(VkNone, None)
+            .build(),
+    );
+
+    //
     // for Neovim
+    //
     // <VK4>+<Key> -> <Leader><Key>
     for key_code in [E, K, F, H, L, O] {
         manipulators.push(
@@ -29,59 +40,6 @@ pub fn manipulators() -> Vec<Manipulator> {
                 .build(),
         );
     }
-
-    // for tmux
-    for key_code in [C, J, K, N, P, S, V] {
-        manipulators.push(
-            Manipulator::builder()
-                .conditions(vec![
-                    Condition::on_app(ITerm2),
-                    Condition::with_virtual_key(Vk4),
-                ])
-                .from_key(key_code.clone())
-                .to_key(T, Some(CTRL.to_vec()))
-                .to_key(key_code, Some(CTRL.to_vec()))
-                .build(),
-        )
-    }
-
-    // Disable default Cmd+W behavior in iTerm2
-    manipulators.push(
-        Manipulator::builder()
-            .condition(Condition::on_app(ITerm2))
-            .from_key_with_modifiers(W, FromModifier::Mandatory(CMD.to_vec()))
-            .to_key(VkNone, None)
-            .build(),
-    );
-
-    // Pane movement
-    for (from, to) in [(O, P), (P, N)] {
-        manipulators.push(
-            Manipulator::builder()
-                .conditions(vec![
-                    Condition::on_app(ITerm2),
-                    Condition::with_virtual_key(Vk1),
-                ])
-                .from_key(from)
-                .to_key(T, Some(CTRL.to_vec()))
-                .to_key(to, Some(CTRL.to_vec()))
-                .build(),
-        )
-    }
-    for (from, to) in [(A, P), (S, N)] {
-        manipulators.push(
-            Manipulator::builder()
-                .conditions(vec![
-                    Condition::on_app(ITerm2),
-                    Condition::with_virtual_key(Vk2),
-                ])
-                .from_key(from)
-                .to_key(T, Some(CTRL.to_vec()))
-                .to_key(to, Some(CTRL.to_vec()))
-                .build(),
-        )
-    }
-
     // save file in vim
     manipulators.push(
         Manipulator::builder()
@@ -96,7 +54,6 @@ pub fn manipulators() -> Vec<Manipulator> {
             .to_key(ReturnOrEnter, None)
             .build(),
     );
-
     // quit vim
     manipulators.push(
         Manipulator::builder()
@@ -111,10 +68,11 @@ pub fn manipulators() -> Vec<Manipulator> {
             .to_key(ReturnOrEnter, None)
             .build(),
     );
-
-    // U -> Shift+0 (go to line head in vim)
-    // I -> Shift+4 (go to line tail in vim)
-    for (from, to) in [(U, Key0), (I, Key4)] {
+    // go to line head/tail in vim
+    for (from, to) in [
+        (U, Key0), // go to line head in vim
+        (I, Key4), // go to line tail in vim
+    ] {
         manipulators.push(
             Manipulator::builder()
                 .conditions(vec![
@@ -127,18 +85,50 @@ pub fn manipulators() -> Vec<Manipulator> {
         )
     }
 
-    // what is this?
-    manipulators.push(
-        Manipulator::builder()
-            .conditions(vec![
-                Condition::on_app(ITerm2),
-                Condition::with_virtual_key(Vk1),
-            ])
-            .from_key(Semicolon)
-            .to_key(F, Some(CTRL.to_vec()))
-            .build(),
-    );
-
+    //
+    // for tmux
+    //
+    for key_code in [C, J, K, N, P, S, V] {
+        manipulators.push(
+            Manipulator::builder()
+                .conditions(vec![
+                    Condition::on_app(ITerm2),
+                    Condition::with_virtual_key(Vk4),
+                ])
+                .from_key(key_code.clone())
+                .to_key(T, Some(CTRL.to_vec()))
+                .to_key(key_code, Some(CTRL.to_vec()))
+                .build(),
+        )
+    }
+    // Pane movement with O and P in tmux
+    for (from, to) in [(O, P), (P, N)] {
+        manipulators.push(
+            Manipulator::builder()
+                .conditions(vec![
+                    Condition::on_app(ITerm2),
+                    Condition::with_virtual_key(Vk1),
+                ])
+                .from_key(from)
+                .to_key(T, Some(CTRL.to_vec()))
+                .to_key(to, Some(CTRL.to_vec()))
+                .build(),
+        )
+    }
+    // Pane movement with A and S in tmux
+    for (from, to) in [(A, P), (S, N)] {
+        manipulators.push(
+            Manipulator::builder()
+                .conditions(vec![
+                    Condition::on_app(ITerm2),
+                    Condition::with_virtual_key(Vk2),
+                ])
+                .from_key(from)
+                .to_key(T, Some(CTRL.to_vec()))
+                .to_key(to, Some(CTRL.to_vec()))
+                .build(),
+        )
+    }
     // Enter copy mode in tmux
     manipulators.push(
         Manipulator::builder()
@@ -149,6 +139,18 @@ pub fn manipulators() -> Vec<Manipulator> {
             .from_key(Z)
             .to_key(T, Some(CTRL.to_vec()))
             .to_key(B, Some(CTRL.to_vec()))
+            .build(),
+    );
+
+    // what is this?
+    manipulators.push(
+        Manipulator::builder()
+            .conditions(vec![
+                Condition::on_app(ITerm2),
+                Condition::with_virtual_key(Vk1),
+            ])
+            .from_key(Semicolon)
+            .to_key(F, Some(CTRL.to_vec()))
             .build(),
     );
 
