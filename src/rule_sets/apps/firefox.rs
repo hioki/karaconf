35b288bd08df01e079e1a2f387a3d4b788175cbc
Daichi Hioki <hioki.daichi@gmail.com>
2026-07-07 14:24:43 +0900

    firefox

--- src/rule_sets/apps/firefox.rs
+++ src/rule_sets/apps/firefox.rs
@@ -20,5 +20,14 @@ pub fn manipulators() -> Vec<Manipulator> {
             .from_key(E)
             .to_key(Z, Some(vec![Ctrl]))
             .build(),
+        Manipulator::builder()
+            .conditions(vec![
+                Condition::on_app(Firefox),
+                Condition::with_virtual_key(Vk4),
+            ])
+            .from_key(R)
+            .to_key(L, Some(vec![Cmd]))
+            .to_key(EqualSign, None)
+            .build(),
     ]
 }
