46351e80c563db6e0f8f0296c165ec114fff19ae
Daichi Hioki <hioki.daichi@gmail.com>
2026-08-20 15:58:35 +0900

    Update copilot mappings

--- src/rule_sets/apps/github_copilot.rs
+++ src/rule_sets/apps/github_copilot.rs
@@ -7,14 +7,15 @@ pub fn manipulators() -> Vec<Manipulator> {
     for (description, from_key, (to_key, modifiers)) in [
         ("View keyboard shortcuts", Slash, (Slash, Some(vec![Shift]))),
         ("Command palette", A, (K, Some(vec![Cmd]))),
-        ("Find in file", F, (F, Some(vec![Cmd]))),
-        ("Next session", N, (G, Some(vec![Cmd]))),
-        ("Previous session", P, (P, Some(vec![Cmd, Shift]))),
+        ("Find in file", K, (F, Some(vec![Cmd]))),
+        ("Next session", Key2, (G, Some(vec![Cmd]))),
+        ("Previous session", Key1, (P, Some(vec![Cmd, Shift]))),
         ("Go back", H, (CloseBracket, Some(vec![Cmd]))),
-        ("Go forward", H, (NonUsPound, Some(vec![Cmd]))),
+        ("Go forward", L, (NonUsPound, Some(vec![Cmd]))),
         ("Toggle sidebar", E, (B, Some(vec![Cmd]))),
-        ("Go to file", K, (P, Some(vec![Cmd]))),
+        ("Go to file", F, (P, Some(vec![Cmd]))),
         ("New chat", N, (O, Some(vec![Cmd, Shift]))),
+        ("Toggle review panel", J, (B, Some(vec![Cmd, Opt]))),
     ] {
         manipulators.push(
             Manipulator::builder()
